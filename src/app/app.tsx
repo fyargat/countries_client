e82606cd5dfdd3ffc7d75a8a6e79750603c79508
Cass <bikbaevf18@gmail.com>
2023-10-24 20:18:30 +0400

    Add styles base

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,6 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+
+import './index.scss';
 
 export const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
