75a71019f11bfae5785e5acd021790e8b3017e94
Cass <bikbaevf18@gmail.com>
2023-10-24 20:54:25 +0400

    Add api module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
 
-module.exports = nextConfig
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://api:3000/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
