73d3f867b3c40d4d6e3e3bacb1380459a50190da
Cass <bikbaevf18@gmail.com>
2023-10-26 15:11:58 +0400

    Add favicon & title

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import './index.scss';
 import { AppProvider } from './provider';
@@ -6,6 +7,12 @@ import { AppProvider } from './provider';
 export const App = ({ Component, pageProps }: AppProps) => {
   return (
     <AppProvider>
+      <Head>
+        <meta name='keywords' content='Countries, meta, nextjs' />
+        <meta name='author' content='Fyargat' />
+        <title>Countries</title>
+        <meta property='og:title' content='Countries' key='title' />
+      </Head>
       <Component {...pageProps} />
     </AppProvider>
   );
